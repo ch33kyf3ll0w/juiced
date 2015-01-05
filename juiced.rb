ce3876e01644a8fcc402c7704a4ed9f9d60f646c
ch33kyf3ll0w <ch33kyf3ll0w@users.noreply.github.com>
2015-01-05 05:53:15 -0600

    Update juiced.rb

--- juiced.rb
+++ juiced.rb
@@ -1,5 +1,6 @@
 #!/usr/bin/env ruby
-#Author: Andrew Bonstrom, 
+#Author: Andrew Bonstrom
+#v1.1
 #Credits to: Matthew Graber - Beastly PS Attack technique, TrustedSec group - Idea with Unicorn.py
 require 'io/console'
 require 'base64'
